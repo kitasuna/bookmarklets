d42c2cc72add778f42cdb9a412e3db191e31bcb1
kitasuna <onwarmermusic@gmail.com>
2016-07-13 05:28:56 +0900

    add some new langs and comments indicating which language code maps to which language

--- randomwikilang.js
+++ randomwikilang.js
@@ -6,6 +6,26 @@
  */
 
 (function(){
-	var langs = ["en","sv","ceb","de","nl","fr","ru","war","it","es","pl","vi","ja"];
+	var langs = [
+		"ca",	/*Catalan*/
+		"ceb",	/*Cebuano*/
+		"de",	/*German*/
+		"en",	/*English*/
+		"es",	/*Spanish*/
+		"fa",	/*Persian*/
+		"fr",	/*French*/
+		"it",	/*Italian*/
+		"ja",	/*Japanese*/
+		"nl",	/*Norweigian*/
+		"no",	/*Norwegian (Bokmal)*/
+		"pl",	/*Polish*/
+		"pt",	/*Portuguese*/
+		"ru",	/*Russian*/
+		"sv",	/*Swedish*/
+		"uk",	/*Ukranian*/
+		"war",	/*Winaray*/
+		"vi",	/*Vietnamese*/
+		"zh",	/*Chinese*/
+		];
 	window.location.href = 'http://'+langs[Math.floor(Math.random()*langs.length)]+'.wikipedia.org';
 })();
